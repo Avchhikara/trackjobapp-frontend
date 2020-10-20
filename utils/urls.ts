7892c4bdf7d2b471b0fc51b81f5873d01957e177
Avnish <Avnishway@gmail.com>
2020-10-20 15:30:06 +0530

    Adding URL for new backend

--- utils/urls.ts
+++ utils/urls.ts
@@ -1,4 +1,4 @@
-export const BASE_URL: string = process.env.NODE_ENV !== "production" ? "http://localhost:8080": "https://trackjobapp.herokuapp.com";
+export const BASE_URL: string = process.env.NODE_ENV !== "production" ? "http://localhost:8080": "https://trackjobapp-backend.herokuapp.com";
 export const ENTRIES_URL: string = `${BASE_URL}/entries`;
 export const CREATE_ENTRY_URL: string = `${BASE_URL}/add`;
 export const LOGIN_URL: string = `${BASE_URL}/login`;
